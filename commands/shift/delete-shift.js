74657c3e202529449f599a89dfe7d2bf4c15438a
pbawane899 <pbawane899@gmail.com>
2025-10-03 12:14:19 +0000

    Added new command to delete stuff

---
+++ commands/shift/delete-shift.js
@@ -0,0 +1,29 @@
+const { InteractionContextType, ButtonBuilder, ButtonStyle, SlashCommandBuilder, EmbedBuilder, MessageFlags, ActionRowBuilder } = require('discord.js');
+const { shiftBroadcastChannel, shiftAdminRole, devUser, onShiftRole, onBreakRole } = require('../../config.json');
+const Shift = require('../../utility/shift-handle');
+const { nanoid } = require('nanoid');
+const moment = require('moment');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('delete-shift')
+        .setDescription('Delete an active shift')
+        .addStringOption(option =>
+            option
+                .setName('id')
+                .setDescription('Id of the shift')
+                .setRequired(true)),
+    async execute(interaction) {
+        if (!interaction.member.roles.cache.has(shiftAdminRole) && interaction.user.id !== devUser) {
+            return interaction.reply({ content: 'You do not have permission to use this command!', flags: MessageFlags.Ephemeral });
+        }
+
+        const id = interaction.options.getString('id');
+        try {
+            await Shift.delete(id);
+            interaction.reply({ content: `Shift deleted!`, flags: MessageFlags.Ephemeral });
+        } catch (error) {
+            return interaction.reply({ content: 'Something went wrong, try again later...', flags: MessageFlags.Ephemeral });
+        }
+    },
+};
