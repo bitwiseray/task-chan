9a2b1a126130f0c5fd511ece9d3bca72068455a4
pbawane899 <pbawane899@gmail.com>
2025-10-03 12:23:55 +0000

    Added extra checks

--- commands/shift/delete-shift.js
+++ commands/shift/delete-shift.js
@@ -15,15 +15,21 @@ module.exports = {
                 .setRequired(true)),
     async execute(interaction) {
         if (!interaction.member.roles.cache.has(shiftAdminRole) && interaction.user.id !== devUser) {
-            return interaction.reply({ content: 'You do not have permission to use this command!', flags: MessageFlags.Ephemeral });
+            return interaction.reply({ content: '⚠️ You do not have permission to use this command!', flags: MessageFlags.Ephemeral });
         }
 
         const id = interaction.options.getString('id');
+        let title;
+        const shift = await Shift.get(id);
+        if (!shift) {
+            return interaction.reply({ content: '❌ Shift not found!', flags: MessageFlags.Ephemeral });
+        }
+        title = shift.title;
         try {
             await Shift.delete(id);
-            interaction.reply({ content: `Shift deleted!`, flags: MessageFlags.Ephemeral });
+            interaction.reply({ content: `✅ **${title}** shift deleted!`, flags: MessageFlags.Ephemeral });
         } catch (error) {
-            return interaction.reply({ content: 'Something went wrong, try again later...', flags: MessageFlags.Ephemeral });
+            return interaction.reply({ content: '❌ Something went wrong, try again later...', flags: MessageFlags.Ephemeral });
         }
     },
 };
