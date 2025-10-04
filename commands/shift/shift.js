6adab2efd0eb8cab18726d47ac1c9dcc012d7b0e
pbawane899 <pbawane899@gmail.com>
2025-10-04 09:39:08 +0000

    Finished the shift details command

---
+++ commands/shift/shift.js
@@ -0,0 +1,62 @@
+const { SlashCommandBuilder, EmbedBuilder, MessageFlags, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const Shift = require('../../utility/shift-handle');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('shift')
+        .setDescription('Display your current shift details.'),
+    async execute(interaction) {
+        const shift = await Shift.getShiftByUser(interaction.user.id);
+        const key = shift.id;
+        const statusEmojis = {
+            STARTED: "✅",
+            REJECTED: "❌",
+            PENDING: "🟡"
+        };
+        const color = {
+            STARTED: '#43B581',
+            REJECTED: '#F04747',
+            PENDING: '#FAA61A'
+        }
+
+        if (!shift) {
+            await interaction.reply({ content: 'You are not currently on a shift.', flags: MessageFlags.Ephemeral });
+            return;
+        }
+
+        const embed = new EmbedBuilder()
+            .setAuthor({ name: interaction.user.displayName, iconURL: interaction.user.avatarURL() })
+            .setColor(color[shift.status])
+            .setTitle(`📋 Current Task: ${shift.title}`)
+            .setDescription(`**${shift.details}**
+                
+                - **Status**: ${statusEmojis[shift.status] || '❔'} ${shift.status}
+                - **Assigned to**: <@${shift.assignedId}>
+                - **Started**: <t:${Math.floor(shift.startedAt / 1000)}:R>
+                - **Deadline**: <t:${Math.floor(shift.deadline / 1000)}:R>
+                - **Task ID**: ${shift.id}`)
+            .setTimestamp()
+            .setFooter({ text: interaction.guild.name });
+        
+            const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId(`completeShift:${key}`)
+                    .setLabel('Complete')
+                    .setEmoji('✅')
+                    .setStyle(ButtonStyle.Secondary),
+                new ButtonBuilder()
+                    .setCustomId(`pauseShift:${key}`)
+                    .setLabel('Pause')
+                    .setEmoji('⏳')
+                    .setStyle(ButtonStyle.Secondary),
+                new ButtonBuilder()
+                    .setCustomId(`endShift:${key}`)
+                    .setLabel('End')
+                    .setEmoji('⛔')
+                    .setStyle(ButtonStyle.Secondary)
+            );
+
+        await interaction.reply({ embeds: [embed], components: [row] });
+    },
+};
